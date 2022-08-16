d02e5543bdf6f55edc6c5893c4683d5d78a7d882
Z7LL <108791502+Z7LL@users.noreply.github.com>
2022-08-16 23:59:06 +0400

    Create Nitro gen bot.py
    Go to Line 45 and change the Bot Token.

---
+++ Nitro gen bot.py
@@ -0,0 +1,45 @@
+from itertools import count
+from multiprocessing.connection import Client
+import string
+import random
+import discord
+ 
+client = discord.Client()
+ 
+@client.event
+async def on_ready():
+    print('we have logged in as {0.user}' . format(client))
+ 
+ 
+Key_Len = 24
+ 
+ 
+Link = 'https://discord.gift/'
+ 
+Generator = (string.ascii_letters + string.digits)#This is generator's base
+Generator1 = [random.choice(Generator)for i in range(Key_Len)]
+Generator2 = (''.join(Generator1))
+  
+
+print(Generator2)
+ 
+@client.event
+async def on_message(message):
+   if message.content.startswith('$'): 
+     await message.channel.send(Link + Generator2)
+
+print
+
+@client.event
+async def on_message(message):
+   if message.content.startswith('$'): 
+     count = 0
+     while count < 1:
+         await message.channel.send(Link + Generator2)
+
+
+
+
+
+
+client.run('BOT-TOKEN-HERE')
